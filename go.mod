440df6cc219c4b523a6ef3485803081fa0aa6456
Peyton-Spencer <pas1234567@gmail.com>
2024-03-27 21:00:51 -0600

    Initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module templ-go-std
+
+go 1.22.1
+
+require (
+	github.com/a-h/templ v0.2.648
+	github.com/joho/godotenv v1.5.1
+	github.com/mattn/go-sqlite3 v1.14.22
+	nhooyr.io/websocket v1.8.10
+)
---
+++ internal/server/routes.go
@@ -0,0 +1,78 @@
+package server
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"fmt"
+	"time"
+
+	"github.com/a-h/templ"
+	"nhooyr.io/websocket"
+	"templ-go-std/cmd/web"
+)
+
+func (s *Server) RegisterRoutes() http.Handler {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", s.HelloWorldHandler)
+
+	mux.HandleFunc("/health", s.healthHandler)
+
+	mux.HandleFunc("/websocket", s.websocketHandler)
+
+	fileServer := http.FileServer(http.FS(web.Files))
+	mux.Handle("/js/", fileServer)
+	mux.Handle("/web", templ.Handler(web.HelloForm()))
+	mux.HandleFunc("/hello", web.HelloWebHandler)
+
+	return mux
+}
+
+func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	resp := make(map[string]string)
+	resp["message"] = "Hello World"
+
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatalf("error handling JSON marshal. Err: %v", err)
+	}
+
+	_, _ = w.Write(jsonResp)
+}
+
+func (s *Server) healthHandler(w http.ResponseWriter, r *http.Request) {
+	jsonResp, err := json.Marshal(s.db.Health())
+
+	if err != nil {
+		log.Fatalf("error handling JSON marshal. Err: %v", err)
+	}
+
+	_, _ = w.Write(jsonResp)
+}
+
+func (s *Server) websocketHandler(w http.ResponseWriter, r *http.Request) {
+	socket, err := websocket.Accept(w, r, nil)
+
+	if err != nil {
+		log.Printf("could not open websocket: %v", err)
+		_, _ = w.Write([]byte("could not open websocket"))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	defer socket.Close(websocket.StatusGoingAway, "server closing websocket")
+
+	ctx := r.Context()
+	socketCtx := socket.CloseRead(ctx)
+
+	for {
+		payload := fmt.Sprintf("server timestamp: %d", time.Now().UnixNano())
+		err := socket.Write(socketCtx, websocket.MessageText, []byte(payload))
+		if err != nil {
+			break
+		}
+		time.Sleep(time.Second * 2)
+	}
+}
